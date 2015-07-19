c6dd69dba2beeb0e83a7a4269742eeb861441acd
yanlong <scicodist@gmail.com>
2015-07-20 00:00:57 +0800

    auto inc actions for ugc posts

--- server/crontab.js
+++ server/crontab.js
@@ -70,3 +70,47 @@ function weights(posts, now) {
         };
     })
 }
+
+SyncedCron.add({
+    name: 'auto inc actions for ugc',
+    schedule: function(parser) {
+        // parser is a later.parse object
+        return parser.text('every 42 seconds');
+        // return parser.text('every 1 minutes');
+        // return parser.text('at 22:00');
+    },
+    job: function() {
+        var selector = {
+            status: {
+                $nin: ['deleted', 'forbidden']
+            },
+            device: {
+                $ne: 'wtfspot',
+            }
+        };
+
+        Posts.find(selector, {fields: {content:0}}).forEach(function (post) {
+            var now = Date.now();
+            var ctime = post.ctime;
+            var gap = (now - ctime) / 1000;
+            if (gap > 3600) {
+                return;
+            }
+            var base = 6 - gap / 600;
+            var primary = ctime % (post.actions.length + 1);
+            var actions = post.actions.concat(['_light']);
+            var times = [ctime % 42] / 10
+            var delta = ctime % 6;
+            var query = {
+            };
+            for (var i = 0; i < actions.length; i++) {
+                var rand = Math.floor(Math.random()* 100) % 6;
+                var inc = (base + delta) * (i == primary ? times: 1) + rand;
+                query['nActions.'+actions[i]] = Math.floor(inc);
+            }
+            logger.info('auto inc: ', post._id, query, times);
+            Posts.update(post._id, {$inc: query});
+            return ;
+        })
+    }
+});
